59ff6320a98b261d4d656753883c64cb89333cb2
Ben Blake <benjamin.blake@shopify.com>
2019-05-02 19:35:19 -0400

    Beautifying some code

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,16 +1,16 @@
-import React from "react-native";
-import {Text as RNText, TextProps as RNTextProps} from "react-native"
 import { ReactNode } from "react";
+import React from "react-native";
+import { Text as RNText, TextProps as RNTextProps } from "react-native";
 import * as globalStyles from "../../style/global";
 
 interface Props extends RNTextProps {
   children?: ReactNode;
 }
 
-export const Text = ({children, style, ...rest}: Props) => {
+export const Text = ({ children, style, ...rest }: Props) => {
   return (
     <RNText style={[globalStyles.COMMON_STYLES.text, style]} {...rest}>
       {children}
     </RNText>
   );
-}
+};
