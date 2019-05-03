ee88c1e2162ef081f01e1f121829e23aa3989505
Ben Blake <benjamin.blake@shopify.com>
2019-05-02 21:25:09 -0400

    Adding additional Text stories

--- src/components/Text/__stories__/Text.story.tsx
+++ src/components/Text/__stories__/Text.story.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 import { storiesOf } from "@storybook/react-native";
+import { View, ViewProps } from "react-native";
 import { Text } from "../Text";
 
-storiesOf("Text", module).add("default view", () => (
-  <Text>Hello Storybook</Text>
+interface Props extends ViewProps {
+  children?: ReactNode;
+}
+const Container = ({ children, style, ...rest }: Props) => {
+  return (
+    <View style={[{ padding: 20 }, style]} {...rest}>
+      {children}
+    </View>
+  );
+};
+
+storiesOf("Text", module).add("all types", () => (
+  <View style={{ flex: 1, alignItems: "center" }}>
+    <Container>
+      <Text>Hello Storybook</Text>
+    </Container>
+    <Container style={{ backgroundColor: "#000" }}>
+      <Text>Hello Storybook</Text>
+    </Container>
+  </View>
 ));
