ab921248330280f84419b1ce30e6908dc73ed812
Ben Blake <benjamin.blake@shopify.com>
2019-05-26 21:38:08 -0400

    Adding NewsFeed component to the HomeScreen

--- src/components/HomeScreen/HomeScreen.tsx
+++ src/components/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { NewsFeed } from "../NewsFeed/NewsFeed";
 import { TabBar } from "./components";
 
 interface State {
@@ -14,13 +15,30 @@ export class HomeScreen extends Component<{}, State> {
   };
 
   public render() {
+    const { tab } = this.state;
+
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ flex: 1 }} />
-        <TabBar selectedTab={this.state.tab} onTabSelected={this.onTabChange} />
+        <View style={{ flex: 1 }}>{this.renderScreen()}</View>
+        <TabBar selectedTab={tab} onTabSelected={this.onTabChange} />
       </View>
     );
   }
+
+  private renderScreen = () => {
+    const { tab } = this.state;
+
+    switch (tab) {
+      case tabs[0]:
+        return <NewsFeed />;
+      case tabs[1]:
+        return <Text>Search</Text>;
+      case tabs[2]:
+        return <Text>Bookmarks</Text>;
+      default:
+        return null;
+    }
+  };
 
   private onTabChange = (tab: string) => {
     this.setState({ tab });
--- src/components/NewsFeed/NewsFeed.tsx
+++ src/components/NewsFeed/NewsFeed.tsx
@@ -23,6 +23,20 @@ interface State {
 }
 
 export class NewsFeed extends PureComponent<Props, State> {
+  public static defaultProps = {
+    news: Array(10).fill({
+      author: "Shandy B",
+      date: new Date(),
+      index: 0,
+      style: {},
+      onPress: () => {},
+      title: "Levi the dbag",
+      imageUrl:
+        "https://avatars1.githubusercontent.com/u/17188936?s=400&u=cfea6044dcd4e480dc8ca09f7c73e1996e116e54&v=4",
+      url: "https://www.cbc.ca/",
+    }),
+  };
+
   public state: State = {
     modalVisible: false,
     modalUrl: "",
@@ -62,7 +76,10 @@ export class NewsFeed extends PureComponent<Props, State> {
           >
             <SmallText>Close</SmallText>
           </TouchableOpacity>
-          <WebView scalesPageToFit={true} source={{ uri: this.state.modalUrl }} />
+          <WebView
+            scalesPageToFit={true}
+            source={{ uri: this.state.modalUrl }}
+          />
         </View>
       </Modal>
     );
